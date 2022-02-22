22be072e0f3f49892dc3ace34060a5a24b14e9dc
ueda <ueda@cp.prec.eng.osaka-u.ac.jp>
2022-02-22 20:13:47 +0900

    pylint

---
+++ lint_test.py
@@ -0,0 +1,17 @@
+from numpy import *
+
+def CAPITAL():    
+    UEDA = 'test'
+    print(UEDA)
+    
+    
+    
+maximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelengthmaximumlinelength = ''
+    
+    
+    
+    
+    
+    
+    
+    
